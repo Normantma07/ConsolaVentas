2173a77d31b1ef90f09eba31c79135079c95ad97
Norman <normantma@hotmail.com>
2021-02-28 20:42:18 -0600

    proyecto avance 1

---
+++ src/consolaventas/Menu.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consolaventas;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Cesia Coto
+ */
+public class Menu {
+     public static void main(String[] args) {
+    System.out.println("Bienvenido" + "  \n"+ "¿Que desea hacer?\n");
+            System.out.println("1. Venta Nueva");
+            System.out.println("2. Consultar inventarios");
+            System.out.println("3. Cambiar contraseña");
+            System.out.println("4. Salir del Sistema");
+            System.out.print("Digite la opcion: ");
+    }
+}
---
+++ src/consolaventas/login.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consolaventas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cesia Coto
+ */
+public class login {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+    System.out.println ( "Bienvenido al sistema de ventas Don Diego\n" +
+                        "Favor ingresar credenciales\n");
+   
+    System.out.println ("Ingresar Usuario:\n");
+
+    System.out.println ("Ingresar Contraseña:\n");
+    }
+    
+}
